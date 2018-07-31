967c10778a732e2f917cd1251bcf985464b7ee12
ozaki <fs65ugumori@yahoo.co.jp>
2018-08-01 07:51:58 +0900

    add counter

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,6 @@ module.exports = {
   title: 'ReactHandson',
   base: '/react_handson/',
   themeConfig: {
-    sidebar: ['/1_setup', '/2_initialfile', '/3_hello'],
+    sidebar: ['/1_setup', '/2_initialfile', '/3_hello', '/4_counter'],
   },
 };
