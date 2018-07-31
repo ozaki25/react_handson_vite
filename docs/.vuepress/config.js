2b8f565ea1e9329389b1d2ebdfd4846bfeca8ff6
ozaki <fs65ugumori@yahoo.co.jp>
2018-08-01 07:59:46 +0900

    add todolist

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,12 @@ module.exports = {
   title: 'ReactHandson',
   base: '/react_handson/',
   themeConfig: {
-    sidebar: ['/1_setup', '/2_initialfile', '/3_hello', '/4_counter'],
+    sidebar: [
+      '/1_setup',
+      '/2_initialfile',
+      '/3_hello',
+      '/4_counter',
+      '/5_todolist',
+    ],
   },
 };
